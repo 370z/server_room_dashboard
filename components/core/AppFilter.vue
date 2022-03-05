87516ffd2de4cc3650489af88f985c2ae9b5be1b
370z <mos-moji350@hotmail.com>
2022-03-06 01:52:37 +0700

    Modify Drawer

--- components/core/AppFilter.vue
+++ components/core/AppFilter.vue
@@ -127,10 +127,6 @@
         'danger'
       ],
       images: [
-        'https://demos.creative-tim.com/vue-material-dashboard/img/sidebar-1.23832d31.jpg',
-        'https://demos.creative-tim.com/vue-material-dashboard/img/sidebar-2.32103624.jpg',
-        'https://demos.creative-tim.com/vue-material-dashboard/img/sidebar-3.3a54f533.jpg',
-        'https://demos.creative-tim.com/vue-material-dashboard/img/sidebar-4.3b7e38ed.jpg'
       ]
     }),
 
